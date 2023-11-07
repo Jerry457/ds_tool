b2d7dfc90a6371bf40b2a449fb67b41690991e16
Jerry <2234834467@qq.com>
2023-11-07 15:27:38 +0800

    fix scml bug

--- ds_file/scml.py
+++ ds_file/scml.py
@@ -114,79 +114,79 @@ class Scml(ElementTree):
 
         frame_duration = 1000 // AnimBank.frame_rate
 
-        # for entity in scml_root.findall("entity"):
-        #     entity_name = entity.get("name")
-        #     anim_data["banks"][entity_name] = {}
+        for entity in scml_root.findall("entity"):
+            entity_name = entity.get("name")
+            anim_data["banks"][entity_name] = {}
 
-        #     for animation in entity.findall("animation"):
-        #         mainline = animation.find("mainline")
+            for animation in entity.findall("animation"):
+                mainline = animation.find("mainline")
 
-        #         print("convert animation: " + (animation_name := animation.get("name")))
-        #         anim_data["banks"][entity_name][animation_name] = {"framerate": AnimBank.frame_rate, "frames": []}
+                print("convert animation: " + (animation_name := animation.get("name")))
+                anim_data["banks"][entity_name][animation_name] = {"framerate": AnimBank.frame_rate, "frames": []}
 
-        #         mainline_keys = mainline.findall("key")
-        #         last_time = 0
-        #         for mainline_key in mainline_keys:
-        #             time = int(mainline_key.get("time", 0))
-        #             assert (time - last_time) % frame_duration == 0, f"{animation_name} error, The frame duration must be {frame_duration}"
-        #             last_time = time
+                mainline_keys = mainline.findall("key")
+                last_time = 0
+                for mainline_key in mainline_keys:
+                    time = int(mainline_key.get("time", 0))
+                    assert (time - last_time) % frame_duration == 0, f"{animation_name} error, The frame duration must be {frame_duration}"
+                    last_time = time
 
-        #             Frame = {"elements": []}
+                    Frame = {"elements": []}
 
-        #             frame_verts = []
-        #             frame_positions = []
+                    frame_verts = []
+                    frame_positions = []
 
-        #             object_refs = mainline_key.findall("object_ref")
-        #             object_ref_cont = len(object_refs)
-        #             for object_ref in object_refs:
-        #                 timeline = animation.find(f"timeline[@id='{object_ref.get('timeline')}']")
-        #                 timeline_key = timeline.find(f"key[@id='{object_ref.get('key')}']")
-        #                 timeline_key_object = timeline_key.find("object")
-        #                 file = scml_root.find(f"folder[@id='{timeline_key_object.get('folder')}']/file[@id='{timeline_key_object.get('file')}']")
+                    object_refs = mainline_key.findall("object_ref")
+                    object_ref_cont = len(object_refs)
+                    for object_ref in object_refs:
+                        timeline = animation.find(f"timeline[@id='{object_ref.get('timeline')}']")
+                        timeline_key = timeline.find(f"key[@id='{object_ref.get('key')}']")
+                        timeline_key_object = timeline_key.find("object")
+                        file = scml_root.find(f"folder[@id='{timeline_key_object.get('folder')}']/file[@id='{timeline_key_object.get('file')}']")
 
-        #                 pivot_x, pivot_y = float(file.get("pivot_x")), float(file.get("pivot_y"))
-        #                 w, h = float(file.get("width")), float(file.get("height"))
+                        pivot_x, pivot_y = float(file.get("pivot_x")), float(file.get("pivot_y"))
+                        w, h = float(file.get("width")), float(file.get("height"))
 
-        #                 element_name = scml_root.find(f"folder[@id='{timeline_key_object.get('folder', '0')}']").get("name")
-        #                 layer_name = re.sub(r"_\d+$", "", timeline.get("name", ""))
-        #                 frame_num = int(timeline_key_object.get("file", "0"))
+                        element_name = scml_root.find(f"folder[@id='{timeline_key_object.get('folder', '0')}']").get("name")
+                        layer_name = re.sub(r"_\d+$", "", timeline.get("name", ""))
+                        frame_num = int(timeline_key_object.get("file", "0"))
 
-        #                 pivot_x, pivot_y = float(timeline_key_object.get("pivot_x", pivot_x)), float(timeline_key_object.get("pivot_y", pivot_y))
-        #                 position_x, position_y = float(timeline_key_object.get("x", 0)), float(timeline_key_object.get("y", 0))
-        #                 scale_x, scale_y = float(timeline_key_object.get("scale_x", 1)), float(timeline_key_object.get("scale_y", 1))
-        #                 angle = math.radians(float(timeline_key_object.get("angle", 0)))
-        #                 z_index = object_ref_cont - int(object_ref.get("z_index")) - 1
+                        pivot_x, pivot_y = float(timeline_key_object.get("pivot_x", pivot_x)), float(timeline_key_object.get("pivot_y", pivot_y))
+                        position_x, position_y = float(timeline_key_object.get("x", 0)), float(timeline_key_object.get("y", 0))
+                        scale_x, scale_y = float(timeline_key_object.get("scale_x", 1)), float(timeline_key_object.get("scale_y", 1))
+                        angle = math.radians(float(timeline_key_object.get("angle", 0)))
+                        z_index = object_ref_cont - int(object_ref.get("z_index")) - 1
 
-        #                 frame_positions.append((position_x, position_y))
+                        frame_positions.append((position_x, position_y))
 
-        #                 verts = []
-        #                 verts.append((position_x - pivot_x * w, position_y - (1 - pivot_y) * h))
-        #                 verts.append((position_x + (1 - pivot_x) * w, position_y - (1 - pivot_y) * h))
-        #                 verts.append((position_x - pivot_x * w, position_y + pivot_y * h))
-        #                 verts.append((position_x + (1 - pivot_x) * w, position_y + pivot_y * h))
+                        verts = []
+                        verts.append((position_x - pivot_x * w, position_y - (1 - pivot_y) * h))
+                        verts.append((position_x + (1 - pivot_x) * w, position_y - (1 - pivot_y) * h))
+                        verts.append((position_x - pivot_x * w, position_y + pivot_y * h))
+                        verts.append((position_x + (1 - pivot_x) * w, position_y + pivot_y * h))
 
-        #                 for vert in verts:
-        #                     vert = rotate_point(vert[0], vert[1], position_x, position_y, angle)
-        #                     vert = scale_point(vert[0], vert[1], position_x, position_y, scale_x, scale_y)
-        #                     frame_verts.append(vert)
+                        for vert in verts:
+                            vert = rotate_point(vert[0], vert[1], position_x, position_y, angle)
+                            vert = scale_point(vert[0], vert[1], position_x, position_y, scale_x, scale_y)
+                            frame_verts.append(vert)
 
-        #                 matrix = create_trans_rot_scale_pivot_matrix((position_x, -position_y), angle, (scale_x, scale_y), (0, 0))
-        #                 m = matrix.matrix
+                        matrix = create_trans_rot_scale_pivot_matrix((position_x, -position_y), angle, (scale_x, scale_y), (0, 0))
+                        m = matrix.matrix
 
-        #                 Frame["elements"].append({"name": element_name, "frame": frame_num, "layername": layer_name, "m_a": m[0][0], "m_b": m[1][0], "m_c": m[0][1], "m_d": m[1][1], "m_tx": m[0][2], "m_ty": m[1][2], "z_index": z_index})
+                        Frame["elements"].append({"name": element_name, "frame": frame_num, "layername": layer_name, "m_a": m[0][0], "m_b": m[1][0], "m_c": m[0][1], "m_d": m[1][1], "m_tx": m[0][2], "m_ty": m[1][2], "z_index": z_index})
 
-        #             x_vert = [vert[0] for vert in frame_verts]
-        #             y_vert = [vert[1] for vert in frame_verts]
-        #             left, right, up, down = min(x_vert) if x_vert else 0, max(x_vert) if x_vert else 0, min(y_vert) if y_vert else 0, max(y_vert) if y_vert else 0
-        #             frame_pivot_x = sum([position[0] for position in frame_positions]) / len(frame_positions) if frame_positions else 0
-        #             frame_pivot_y = sum([position[1] for position in frame_positions]) / len(frame_positions) if frame_positions else 0
-        #             frame_w, frame_h = right - left, down - up
-        #             frame_x = frame_w / 2 - (frame_pivot_x - left)
-        #             frame_y = frame_h / 2 - (frame_pivot_y - up)
-        #             Frame["x"], Frame["y"], Frame["w"], Frame["h"] = frame_x, frame_y, frame_w, frame_h
+                    x_vert = [vert[0] for vert in frame_verts]
+                    y_vert = [vert[1] for vert in frame_verts]
+                    left, right, up, down = min(x_vert) if x_vert else 0, max(x_vert) if x_vert else 0, min(y_vert) if y_vert else 0, max(y_vert) if y_vert else 0
+                    frame_pivot_x = sum([position[0] for position in frame_positions]) / len(frame_positions) if frame_positions else 0
+                    frame_pivot_y = sum([position[1] for position in frame_positions]) / len(frame_positions) if frame_positions else 0
+                    frame_w, frame_h = right - left, down - up
+                    frame_x = frame_w / 2 - (frame_pivot_x - left)
+                    frame_y = frame_h / 2 - (frame_pivot_y - up)
+                    Frame["x"], Frame["y"], Frame["w"], Frame["h"] = frame_x, frame_y, frame_w, frame_h
 
-        #             anim_data["banks"][entity_name][animation_name]["frames"].append(Frame)
-        #             anim_data["banks"][entity_name][animation_name]["numframes"] = len(Frame)
+                    anim_data["banks"][entity_name][animation_name]["frames"].append(Frame)
+                    anim_data["banks"][entity_name][animation_name]["numframes"] = len(Frame)
 
         from ds_file.anim import DSAnim
         with DSAnim() as anim:
